72a8c99f949264483e6805b5eba2b828e4461c4f
dncodes <dneverov@gmail.com>
2024-03-20 22:55:24 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,24 @@ sed -i 's/lns = .*/lns = '$VPN_SERVER_IPV4'/' /etc/xl2tpd/xl2tpd.conf
 sed -i 's/name .*/name '$VPN_USERNAME'/' /etc/ppp/options.l2tpd.client
 sed -i 's/password .*/password '$VPN_PASSWORD'/' /etc/ppp/options.l2tpd.client
 
+# Initial cleanup
+if [ -d /var/run/pluto ]; then
+    rm -rf /var/run/pluto
+fi
+mkdir -p /var/run/pluto
+if [ -d /var/run/xl2tpd ]; then
+    rm -rf /var/run/xl2tpd
+fi
+mkdir -p /var/run/xl2tpd
+if [ -f /var/run/xl2tpd/l2tp-control ]; then
+    rm -f /var/run/xl2tpd/l2tp-control
+fi
+touch /var/run/xl2tpd/l2tp-control
+if [ -f /etc/ipsec.d/*.db ]; then
+    rm -f /etc/ipsec.d/*.db
+fi
+
+
 # startup ipsec tunnel
 ipsec initnss
 sleep 1
